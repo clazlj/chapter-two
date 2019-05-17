9c50699bfa72c5842bab084313bfd1c7a458f73f
caoliang <caoliang@jiangduoduo.com>
2019-05-17 14:11:04 +0800

    2

---
+++ src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -0,0 +1,49 @@
+package org.smart4j.chapter2.service;
+
+import org.smart4j.chapter2.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+public class CustomerService {
+
+    /**
+     * 获取客户列表
+     */
+    public List<Customer> getCustomerList(String keyword) {
+        // TODO: 2019/5/17
+        return null;
+    }
+
+    /**
+     * 获取客户
+     */
+    public Customer getCustomer(long id) {
+        // TODO: 2019/5/17
+        return null;
+    }
+
+    /**
+     * 创建客户
+     */
+    public boolean createCustomer(Map<String, Object> fieldMap) {
+        // TODO: 2019/5/17
+        return false;
+    }
+
+    /**
+     * 更新客户
+     */
+    public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
+        // TODO: 2019/5/17
+        return false;
+    }
+
+    /**
+     * 删除客户
+     */
+    public boolean deleteCustomer(long id) {
+        // TODO: 2019/5/17
+        return false;
+    }
+}
---
+++ src/test/java/org/smart4j/chapter2/test/CustomerServiceTest.java
@@ -0,0 +1,66 @@
+package org.smart4j.chapter2.test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.smart4j.chapter2.model.Customer;
+import org.smart4j.chapter2.service.CustomerService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CustomerServiceTest {
+
+    private final CustomerService customerService;
+
+
+    public CustomerServiceTest() {
+        this.customerService = new CustomerService();
+    }
+
+    @Before
+    public void init() {
+        // TODO: 2019/5/17 初始化数据库
+
+    }
+
+    @Test
+    public void getCustomerListTest() {
+        List<Customer> customerList = customerService.getCustomerList("");
+        Assert.assertEquals(2, customerList.size());
+    }
+
+    @Test
+    public void getCustomerTest() {
+        long id = 1;
+        Customer customer = customerService.getCustomer(id);
+        Assert.assertNotNull(customer);
+    }
+
+    @Test
+    public void createCustomerTest() {
+        Map<String, Object> fieldMap = new HashMap<>();
+        fieldMap.put("name", "customer100");
+        fieldMap.put("contact", "John");
+        fieldMap.put("telephone", "13512345678");
+        boolean result = customerService.createCustomer(fieldMap);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void updateCustomerTest() {
+        long id = 1;
+        Map<String, Object> fieldMap = new HashMap<>();
+        fieldMap.put("contact", "Eric");
+        boolean result = customerService.updateCustomer(id, fieldMap);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void deleteCustomer() {
+        long id = 1;
+        boolean result = customerService.deleteCustomer(id);
+        Assert.assertTrue(result);
+    }
+}
